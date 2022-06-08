e1a1f53ef5bdc265d40f9a4b1929179b29c0957a
Elena <helen57_97@mail.ru>
2022-06-08 16:17:41 +0300

    git

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'classicalmusic',
 ]
 
@@ -57,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#CRISPY_TEMPLATE_PACK = 'bootstrap4'########
+CRISPY_TEMPLATE_PACK = 'bootstrap4'########
 ROOT_URLCONF = 'djangoProject.urls'
 
 TEMPLATES = [
@@ -146,8 +145,8 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'####
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))####
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')####
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')####
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
