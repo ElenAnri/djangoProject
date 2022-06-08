d8b1d3b8a451f649bf94eb2b9e9d8566ba8897a8
Elena <helen57_97@mail.ru>
2022-06-08 10:03:11 +0300

    com

--- index.py
+++ index.py
@@ -1,7 +1,4 @@
-from flask import Flask, Response
-app = Flask(__name__)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+# For a Dango app
+from djangoProject.wsgi import application
+# Replace `django_app` with the appropriate name to point towards your project's
+# wsgi.py file
