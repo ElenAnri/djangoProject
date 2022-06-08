2eead42803eb6405968dfc161d9773693b399b46
Elena <helen57_97@mail.ru>
2022-06-08 19:15:21 +0300

    git

--- classicalmusic/urls.py
+++ classicalmusic/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("composer/", views.ComposerView.as_view()),
     path("composer/<slug:slug>/", views.ComposerDetailView.as_view(), name="composer_detail"),
     path("composer/<slug:parent>/<slug:slug>/", views.PieceOfArtDetailView.as_view(), name="pieceOfArt_detail"),
-    path("composer/borodin/<slug:parent>/<slug:slug>/", views.PerDetailView.as_view(), name="per_detail"),
+    path("composer/mozart/<slug:parent>/<slug:slug>/", views.PerDetailView.as_view(), name="per_detail"),
     path("review/rev/r/<int:pk>/", views.AddReviw.as_view(), name="add_review"),
 
 ]
